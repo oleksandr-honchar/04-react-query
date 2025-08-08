28aedc977aa6a66d5763057838f35e91a781dc86
Oleksandr Honchar <figules@gmail.com>
2025-08-08 19:27:46 +0200

    fix placeholder error

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -27,6 +27,7 @@ export default function App() {
     queryKey: ['movies', query, page],
     queryFn: () => fetchMovies({ query, page }),
     enabled: !!query,
+    placeholderData: (previousData) => previousData
   });
 
   const handleSearchSubmit = (newQuery: string) => {
