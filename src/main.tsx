e8980ec6c61650d8cca5afb41531e98e0e04a812
Oleksandr Honchar <figules@gmail.com>
2025-08-08 16:07:05 +0200

    import modern notmalize

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import 'modern-normalize';
 
 const queryClient = new QueryClient();
 
